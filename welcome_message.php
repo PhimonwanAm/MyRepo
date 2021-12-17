2de295826665a888d5ecd8a00c7cb60c13586d8f
PhimonwanAm <63160254@go.buu.ac.th>
2021-12-17 17:41:32 +0700

    Color

--- welcome_message.php
+++ welcome_message.php
@@ -4,11 +4,11 @@
 <title>font color</title>
 </head>
 <body>
-<font color="red">Color Names</font>
-<font color="rgb(255,99,71)">RGB values</font>
-<font color="#ff6347">HEX values</font>
-<font color="hsl(9,100%,64%)">HSL values</font>
-<font color="rgba(255,99,71,0.5)">RGBA values</font>
-<font color="hsla(8,100%,64%,0.5)">HSLA values</font>
+<font color="red">Color Names</font> <br>
+<font color="rgb(255,99,71)">RGB values</font><br>
+<font color="#ff6347">HEX values</font><br>
+<font color="hsl(9,100%,64%)">HSL values</font><br>
+<font color="rgba(255,99,71,0.5)">RGBA values</font><br>
+<font color="hsla(8,100%,64%,0.5)">HSLA values</font><br>
 </body>
 </html>
