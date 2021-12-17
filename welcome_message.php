3ea09f6191aca717febc55001a97650ea444badc
PhimonwanAm <63160254@go.buu.ac.th>
2021-12-17 17:22:01 +0700

    New Welcome

--- welcome_message.php
+++ welcome_message.php
@@ -1,18 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Heading 1 to 6</title>
+<title>paragraph</title>
 </head>
 <body>
-<h1>Heading 1</h1>
-<h1 align="center">Hello Phimonwan</h1>
-line 1 Naja <br>
-line 2 ไทยได้มั้ย <br>
-<h2>Heading 2</h2>
-<h2 align="center">ใช้ไทยได้ สนุกหล่ะงานนี้ ว๊ากกกกกก</h2>
-<h3>Heading 3</h3>
-<h4>Heading 4</h4>
-<h5>Heading 5</h5>
-<h6>Heading 6</h6>
+This is a paragraph.
+This is a paragraph.
+<p align="center">This is a paragraph.</p>
+<p>This is a paragraph.</p>
 </body>
 </html>
